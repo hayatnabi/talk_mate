6df29d71ee7335c00aad29de0afb336f25fca107
HAYAT NABI <HAYATNABI@ISB.MTBC.COM>
2025-05-05 17:10:16 +0500

    Display actual messages on browser instead of test hardcoated messages, Added partial for individual message of any user, Added partial for displaying flash messages

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -2,7 +2,7 @@ class ChatroomController < ApplicationController
   # before_action :require_user, only: [:index]
 
   def index
-    # @messages = Message.all
+    @messages = Message.all
   end
 
   # def create
