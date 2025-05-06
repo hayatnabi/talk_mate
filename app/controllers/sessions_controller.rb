f15cac333f56e54aeabbcb9d1af671ca4aa0a99c
HAYAT NABI <HAYATNABI@ISB.MTBC.COM>
2025-05-06 10:04:44 +0500

    Changed some redirection paths, added some checks for logged in user and gave alternate options according to it

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
-      redirect_to root_path
+      redirect_to chatroom_path
     else
       flash.now[:error] = "Invalid username or password"
       render "new", status: :unprocessable_entity
